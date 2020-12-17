6d2ee4db2469827a8ed52e794c677c2968fe51f2
Jeff S <jeff.shep777@gmail.com>
2020-12-17 16:54:43 -0700

    Add initial variables and functions

---
+++ main.js
@@ -0,0 +1,109 @@
+//
+////Global variables/querySelectors
+//radio buttons
+// var sideRadioButton = ;
+// var mainRadioButton = ;
+// var dessertRadioButton = ;
+//other buttons
+var letsCookButton = document.querySelector('.lets-cook-button');
+//left section
+
+//right section
+var cookpotImage = document.querySelector('.cookpot');
+
+
+
+
+
+////Event Listeners
+
+letsCookButton.addEventListener('click', showDish);
+
+
+
+////Functions
+//show dish and remove cookpot image
+function showDish() {
+  if ("input = true") {
+    // show recipe
+  } else if ("input = true") {
+    // show recipe
+  } else if ("input = true") {
+    // show recipe
+  } else {
+    //error
+  }
+  removeCookpotImage(); //remove cookpot image
+}
+
+function getDish() {
+  //
+}
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+
+
+
+
+function removeCookpotImage() {
+  cookpotImage.classList.toggle('hidden');
+}
+
+
+
+
+
+
+////Food Arrays
+var sides = [
+  'Miso Glazed Carrots',
+  'Coleslaw',
+  'Garden Salad',
+  'Crispy Potatoes',
+  'Sweet Potato Tots',
+  'Coconut Rice',
+  'Caeser Salad',
+  'Shrimp Summer Rolls',
+  'Garlic Butter Mushrooms',
+  'Hush Puppies'
+]
+
+var mains = [
+  'Spaghetti and Meatballs',
+  'Pineapple Chicken',
+  'Shakshuka',
+  'Thai Yellow Curry',
+  'Bibimbap',
+  'Chicken Parmesean',
+  'Butternut Squash Soup',
+  'BBQ Chicken Burgers',
+  'Ramen',
+  'Empanadas',
+  'Chicken Fried Rice',
+  'Sheet Pan Fajitas',
+  'Margarita Pizza'
+]
+
+var desserts = [
+  'Apple Pie',
+  'Lemon Meringue Pie',
+  'Black Forest Cake',
+  'Banana Bread',
+  'Peach Cobbler',
+  'Cheesecake',
+  'Funfetti Cake',
+  'Baklava',
+  'Flan',
+  'Macarons',
+  'Macaroons',
+  'Chocolate Cupcakes',
+  'Pavlova',
+  'Pumpkin Pie',
+  'Key Lime Pie',
+  'Tart Tatin',
+  'Croissants',
+  'Eclairs'
+]
